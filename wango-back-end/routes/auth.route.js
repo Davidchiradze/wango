53ba676a5ac4b8c9140c65d4747d21b21ec40a05
david-ur <david@unlimited-robotics.com>
2025-05-09 15:31:14 +0400

    first commit

---
+++ wango-back-end/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { login, signUp } from "../controllers/auth.controller.js";
+const router = express.Router();
+
+// router.post("/login", login);
+router.post("/sign-up", signUp);
+router.post("/login", login);
+export default router;
